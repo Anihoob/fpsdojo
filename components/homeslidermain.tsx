bf6cc63e6a56bba957eb4036920bf285a8c4b53b
dastagirS <shroud464@gmail.com>
2023-06-02 02:11:40 +0530

    main slider info (styling)

--- components/homeslidermain.tsx
+++ components/homeslidermain.tsx
@@ -21,9 +21,27 @@ export default function HomeMainSlider() {
       >
         <SwiperSlide className="homemainsliderswiperslide">
             <img src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591822/jujutsu_kaizen_gazruz.jpg" alt="" />
+            <div className="homemainsliderinfo">
+              <h4 className="homemainsliderinfo-name">JUJUTSU KAISEN</h4>
+              <span>
+                <h6>TV</h6>
+                <h6>23min</h6>
+                <h6>16+</h6>
+              </span>
+              <p className="about">Yuuji Itadori searches for the rest of the cursed talisman in order to exorcise himself.</p>
+            </div>
         </SwiperSlide>
         <SwiperSlide className="homemainsliderswiperslide">
             <img src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323307/Frame_3bluelockposter_foyhti.png" alt="" />
+            <div className="homemainsliderinfo">
+              <h4 className="homemainsliderinfo-name">BLUE LOCK</h4>
+              <span>
+                <h6>TV</h6>
+                <h6>23min</h6>
+                <h6>6+</h6>
+              </span>
+              <p className="about">Japan's desire for World Cup glory leads the Japanese Football Association to launch a new training programme.</p>
+            </div>
         </SwiperSlide>
       </Swiper>
     </>
