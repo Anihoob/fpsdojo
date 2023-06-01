37a19817f13320fb8d78d6e87845931a99f505c8
dastagirS <shroud464@gmail.com>
2023-06-02 02:31:55 +0530

    slider info (styling)

--- components/homeslider.tsx
+++ components/homeslider.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -8,29 +8,61 @@ import "swiper/css/pagination";
 import "./components.css";
 
 type Props = {
-  section:string;
-}
+  section: string;
+};
 
-export default function HomeSlider(props:Props) {
+export default function HomeSlider(props: Props) {
   return (
     <>
       <h4 className="homesliderswiper-sec">{props.section}</h4>
       <Swiper
-      spaceBetween={30}
-      slidesPerView={1.5}
+        spaceBetween={30}
+        slidesPerView={1.5}
         className="homesliderswiper"
       >
         <SwiperSlide className="homesliderswiperslide">
-            <img src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png" alt="" />
+          <img
+            src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png"
+            alt=""
+          />
+          <div className="homesliderswiperslide-info">
+            <h4 className="homesliderswiperslide-name">Blue Lock</h4>
+            <hr />
+            <h4>S1</h4>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="homesliderswiperslide">
-            <img src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591823/jujutsu-kaisen_xa1hsz.jpg" alt="" />
+          <img
+            src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591823/jujutsu-kaisen_xa1hsz.jpg"
+            alt=""
+          />
+          <div className="homesliderswiperslide-info">
+            <h4 className="homesliderswiperslide-name">Jujutsu Kaisen</h4>
+            <hr />
+            <h4>S1</h4>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="homesliderswiperslide">
-            <img src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png" alt="" />
+          <img
+            src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png"
+            alt=""
+          />
+          <div className="homesliderswiperslide-info">
+            <h4 className="homesliderswiperslide-name">Blue Lock</h4>
+            <hr />
+            <h4>S1</h4>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="homesliderswiperslide">
-            <img src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591823/jujutsu-kaisen_xa1hsz.jpg" alt="" />
+          <img
+            src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591823/jujutsu-kaisen_xa1hsz.jpg"
+            alt=""
+          />
+          <div className="homesliderswiperslide-info">
+            <h4 className="homesliderswiperslide-name">Jujutsu Kaisen</h4>
+            <hr />
+            <h4>S1</h4>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
