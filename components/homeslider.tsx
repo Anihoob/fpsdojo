e28dcaa3284f90b089a0fa3e2bfaeb4b831bed0a
dastagirS <shroud464@gmail.com>
2023-06-03 15:41:24 +0530

    .

--- components/homeslider.tsx
+++ components/homeslider.tsx
@@ -6,6 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 import "./components.css";
+import Link from "next/link";
 
 type Props = {
   section: string;
@@ -20,28 +21,32 @@ export default function HomeSlider(props: Props) {
         slidesPerView={1.5}
         className="homesliderswiper"
       >
+        <Link href={'/Test'}>
         <SwiperSlide className="homesliderswiperslide">
           <img
             src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png"
             alt=""
-          />
+            />
           <div className="homesliderswiperslide-info">
             <h4 className="homesliderswiperslide-name">Blue Lock</h4>
             <hr />
             <h4>S1</h4>
           </div>
         </SwiperSlide>
+        </Link>
+        <Link href={"/Test"}>
         <SwiperSlide className="homesliderswiperslide">
           <img
             src="https://res.cloudinary.com/dxi9wcchp/image/upload/v1635591823/jujutsu-kaisen_xa1hsz.jpg"
             alt=""
-          />
+            />
           <div className="homesliderswiperslide-info">
             <h4 className="homesliderswiperslide-name">Jujutsu Kaisen</h4>
             <hr />
             <h4>S1</h4>
           </div>
         </SwiperSlide>
+            </Link>
         <SwiperSlide className="homesliderswiperslide">
           <img
             src="https://res.cloudinary.com/dp9icjdvf/image/upload/v1667323320/Frame_4bluelock_2_oilzbq.png"
