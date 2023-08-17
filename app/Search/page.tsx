1f81f11a0c20014d94737ae56af022d3ad209468
dastagirS <shroud464@gmail.com>
2023-08-17 12:21:44 +0530

    final fixe for search

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -5,14 +5,14 @@ import Supabase from "@/thirdparty_req/supabase";
 import Link from "next/link";
 
 type searchCard = {
-  id?: number | string | any;
+  id?: number | string | any ;
   title: string;
   description?: string;
   image?: string;
   name?: string;
   releaseDate?: string;
   cover?: string;
-  type?:string;
+  type?: string;
 };
 
 export default function page() {
@@ -34,66 +34,77 @@ export default function page() {
             title: item.title,
           }));
           const movieData: searchCard[] = movie.map((item: any) => ({
-            title: item.title,
+            title: item.title.replace("movie/", ""),
           }));
           const merge = animeData.concat(movieData);
           const searchResults: searchCard[] = merge.filter((item: searchCard) =>
             item.title.toLowerCase().includes(searchItem.toLowerCase())
           );
-          // Fetch additional information for searchResults
-          const updatedSearchResults = await Promise.all(
-            searchResults.map(async (result) => {
-              try {
-                const res = await fetch(
-                  `https://consument-rouge.vercel.app/anime/gogoanime/info/${result.title}`
-                );
-                const data = await res.json();
-
-                if (data.id === result.title) {
-                  return {
-                    ...result,
-                    id:data.id,
-                    image: data.image,
-                    title: data.title,
-                    releaseDate: data.releaseDate,
-                    type: "anime"
-                  };
-                } else {
-                  // Try fetching from movie API
+          if (searchResults) {
+            const updatedSearchResults = await Promise.all(
+              searchResults.map(async (result) => {
+                try {
                   const res = await fetch(
-                    `https://consument-rouge.vercel.app/movies/flixhq/${result.title}`
+                    `https://consument-rouge.vercel.app/anime/gogoanime/info/${result.title}`
                   );
                   const data = await res.json();
-                  const {results} = data
-                  
-                  const closestMatch = closest(result.title, results.map((lao: any)=> lao.title))
-                  const closestMovie = results.find((bruh:any)=> bruh.title === closestMatch)
-                  const closestMovieId = closestMovie.id
-                  const rez = await fetch(`https://consument-rouge.vercel.app/movies/flixhq/info?id=${closestMovieId}`)
-                  const deta = await rez.json()
-                  
-                  if (deta) {
+
+                  if (data.id === result.title) {
                     return {
                       ...result,
-                      id:deta.id.replace("movie/", ""),
-                      image: deta.image,
-                      name: deta.name,
-                      releaseDate: deta.releaseDate,
-                      cover: deta.cover,
-                      type:"movie"
+                      id: data.id,
+                      image: data.image,
+                      title: data.title,
+                      name:data.title,
+                      releaseDate: data.releaseDate,
+                      type: "anime",
                     };
                   } else {
-                    return result;
-                  }
-                }
-              } catch (error) {
-                console.error(error);
-                return result;
-              }
-            })
-          );
+                    const res = await fetch(
+                      `https://consument-rouge.vercel.app/movies/flixhq/${result.title}`
+                    );
+                    const data = await res.json();
+                    const { results } = data;
+                    const bruh = results.filter((br0:any)=> br0.type === "Movie")
 
-          setAnimecontainer(updatedSearchResults);
+                    const closestMatch = closest(
+                      result.title,
+                      bruh.map((lao: any) => lao.title || lao.id)
+                    );
+                    const closestMovie = bruh.find(
+                      (bruh: any) => bruh.title || bruh.id === closestMatch
+                    );
+                    const closestMovieId = closestMovie.id;
+                    const rez = await fetch(
+                      `https://consument-rouge.vercel.app/movies/flixhq/info?id=${closestMovieId}`
+                    );
+                    const deta = await rez.json();
+
+                    if (deta) {
+                      return {
+                        ...result,
+                        id: deta.id.replace("movie/", ""),
+                        image: deta.image,
+                        title:deta.title,
+                        name: deta.name,
+                        releaseDate: deta.releaseDate,
+                        cover: deta.cover,
+                        type: "movie",
+                      };
+                    } else {
+                      return result;
+                  }
+                  }
+                } catch (error) {
+                  console.error(error);
+                  return result;
+                }
+              })
+            );
+            setAnimecontainer(updatedSearchResults);
+          } else {
+            return;
+          }
         }
       } catch (error) {
         console.error(error);
@@ -118,7 +129,14 @@ export default function page() {
         />
         <div className={Styles.searched}>
           {animecontainer?.map((lol) => (
-            <Link href={lol.type === "anime" ? `/AniDojo/anime/${lol.id}`: `/AniDojo/movie/${lol.id.replace("movie/","")}` } className={Styles.fetchedItem}>
+            <Link
+              href={
+                lol.type === "anime"
+                  ? `/AniDojo/anime/${lol.id}`
+                  : `/AniDojo/movie/${lol.id.replace("movie/", "")}`
+              }
+              className={Styles.fetchedItem}
+            >
               <div className={Styles.fetchedspan}>
                 <img
                   className={Styles.fetchedImg}
@@ -126,7 +144,7 @@ export default function page() {
                 />
                 <span>
                   <h4 className={Styles.fetchedTitle}>
-                    {lol.title || lol.name}
+                    {lol.title}
                   </h4>
                   <h4 className={Styles.fetchedTitle}>{lol.releaseDate}</h4>
                 </span>
