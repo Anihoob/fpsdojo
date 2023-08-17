944b160b874d25d3ae2fb16cbff98854acf16eb5
dastagirS <shroud464@gmail.com>
2023-08-17 21:31:57 +0530

    i lied, this time its final fix for search

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -23,56 +23,65 @@ export default function page() {
   const [animecontainer, setAnimecontainer] = useState<searchCard[] | null>(
     null
   );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   async function fetchSupabase() {
     if (searchItem) {
       try {
         const { data: anime } = await superbase.from("tv_series").select("*");
         const { data: movie } = await superbase.from("movies").select("*");
+        
         if (anime && movie) {
           const animeData: searchCard[] = anime.map((item: any) => ({
             title: item.title,
           }));
+          
           const movieData: searchCard[] = movie.map((item: any) => ({
             title: item.title.replace("movie/", ""),
           }));
+          
           const merge = animeData.concat(movieData);
-          const searchResults: searchCard[] = merge.filter((item: searchCard) =>
+          
+          const filteredResults: searchCard[] = merge.filter((item: searchCard) =>
             item.title.toLowerCase().includes(searchItem.toLowerCase())
           );
-          if (searchResults) {
-            const updatedSearchResults = await Promise.all(
-              searchResults.map(async (result) => {
-                try {
+          
+          const updatedSearchResults: searchCard[] = await Promise.all(
+            filteredResults.map(async (result) => {
+              try {
+                const res = await fetch(
+                  `https://consument-rouge.vercel.app/anime/gogoanime/info/${result.title}`
+                );
+                const data = await res.json();
+                
+                if (data.id === result.title) {
+                  return {
+                    ...result,
+                    id: data.id,
+                    image: data.image,
+                    title: data.title,
+                    name: data.title,
+                    releaseDate: data.releaseDate,
+                    type: "anime",
+                  };
+                } else {
                   const res = await fetch(
-                    `https://consument-rouge.vercel.app/anime/gogoanime/info/${result.title}`
+                    `https://consument-rouge.vercel.app/movies/flixhq/${result.title}`
                   );
                   const data = await res.json();
-                  if (data.id === result.title) {
-                    return {
-                      ...result,
-                      id: data.id,
-                      image: data.image,
-                      title: data.title,
-                      name:data.title,
-                      releaseDate: data.releaseDate,
-                      type: "anime",
-                    };
-                  } else {
-                    const res = await fetch(
-                      `https://consument-rouge.vercel.app/movies/flixhq/${result.title}`
-                    );
-                    const data = await res.json();
-                    const { results } = data;
-                    const bruh = results.filter((br0:any)=> br0.type === "Movie")
-
-                    const closestMatch = closest(
-                      result.title,
-                      bruh.map((lao: any) => lao.title || lao.id)
-                    );
-                    const closestMovie = bruh.find(
-                      (bruh: any) => bruh.title || bruh.id === closestMatch
-                    );
+                  const { results } = data;
+                  const bruh = results.filter((br0: any) => br0.type === "Movie");
+                  
+                  const closestMatch = closest(
+                    result.title,
+                    bruh.map((lao: any) => lao.title || lao.id)
+                  );
+                  
+                  const closestMovie = bruh.find(
+                    (bruh: any) => bruh.title || bruh.id === closestMatch
+                  );
+                  
+                  if (closestMovie) {
                     const closestMovieId = closestMovie.id;
                     const rez = await fetch(
                       `https://consument-rouge.vercel.app/movies/flixhq/info?id=${closestMovieId}`
@@ -84,7 +93,7 @@ export default function page() {
                         ...result,
                         id: deta.id.replace("movie/", ""),
                         image: deta.image,
-                        title:deta.title,
+                        title: deta.title,
                         name: deta.name,
                         releaseDate: deta.releaseDate,
                         cover: deta.cover,
@@ -92,27 +101,37 @@ export default function page() {
                       };
                     } else {
                       return result;
+                    }
+                  } else {
+                    return result;
                   }
-                  }
-                } catch (error) {
-                  console.error(error);
-                  return result;
                 }
-              })
-            );
-            setAnimecontainer(updatedSearchResults);
-          } else {
-            return;
-          }
+              } catch (error) {
+                console.error(error);
+                return result;
+              }
+            })
+          );
+          
+          setAnimecontainer(updatedSearchResults);
+          setIsLoading(false);
         }
       } catch (error) {
         console.error(error);
+        setIsLoading(false);
       }
+    } else {
+      setAnimecontainer([]);
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchSupabase();
+    setIsLoading(true);
+    const timeoutId = setTimeout(() => {
+      fetchSupabase();
+    }, 500); 
+    return () => clearTimeout(timeoutId);
   }, [searchItem]);
 
   return (
