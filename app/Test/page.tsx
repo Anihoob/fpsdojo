05b245df9d9bacc00aab019d5ef479952ecb6a16
dastagirS <shroud464@gmail.com>
2023-08-23 16:11:21 +0530

    single page css testing

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -3,15 +3,16 @@ import malapi from "@/thirdparty_req/anilistapi";
 import anilistapi2 from "@/thirdparty_req/anilistapi2";
 import { useEffect, useState } from "react";
 
+import "./test.css";
+
 interface dataType {
   id: string;
-  title:string;
-  image:string;
-  cover:string;
-  releaseDate:string;
-  description:string;
-  genres:string;
-
+  title: string;
+  image: string;
+  cover: string;
+  releaseDate: string;
+  description: string;
+  genres: string;
 }
 
 export default function page() {
@@ -19,10 +20,10 @@ export default function page() {
   const tempTitle = "BLEACH: Thousand-Year Blood War";
   async function fetchMal() {
     const results = await malapi({ title: tempTitle });
-    const findId = results.find((bruh:any)=> bruh.title === tempTitle)
-    const getId = findId.id
-    const getDetails = await anilistapi2({id: getId})
-    setFetchedanime(getDetails)
+    const findId = results.find((bruh: any) => bruh.title === tempTitle);
+    const getId = findId.id;
+    const getDetails = await anilistapi2({ id: getId });
+    setFetchedanime(getDetails);
   }
 
   useEffect(() => {
@@ -30,15 +31,21 @@ export default function page() {
   }, []);
 
   return (
-    <div>
+    <div className="singlePage-main">
+      <div className="singlePage-options">
+        <span>bruh</span>
+      </div>
       {fetchedanime && (
-        <>
-          <h4 style={{ color: "white" }}>{fetchedanime.title}</h4>
-          <img src={fetchedanime.image} alt="" />
-          <img src={fetchedanime.cover} alt="" />
-          <p style={{ color: "white" }}>{fetchedanime.genres[0]}</p>
-          <p style={{ color: "white" }}>{fetchedanime.description}</p>
-        </>
+        <div className="singlePage-info">
+          <div className="singlePage-poster">
+            <img src={fetchedanime.image} alt="" />
+          </div>
+          <div className="singlePage-card">
+            <div className="singlePage-details">
+              
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
