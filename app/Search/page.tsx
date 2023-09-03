e16c6e354314435bf9c1b1284eece7720bb78dbe
dastagirS <shroud464@gmail.com>
2023-09-03 19:07:54 +0530

    search opti

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -69,7 +69,7 @@ export default function SearchPage() {
   useEffect(() => {
     const Search = setTimeout(() => {
       fetchResults();
-    },350);
+    },200);
 
     return () => clearTimeout(Search);
   }, [searchitem]);
