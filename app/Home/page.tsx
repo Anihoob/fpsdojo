a2256f396aa4062ed802c7d2c3b27d4cb56acc35
dastagirS <shroud464@gmail.com>
2023-10-11 16:21:42 +0530

    added slider line

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <CaroselSlider starting={0} ending={4}/>
         <CaroselSlider starting={5} ending={9}/>
         <CaroselSlider starting={10} ending={14}/>
+        <CaroselSlider starting={15} ending={20}/>
     </div>
   )
 }
