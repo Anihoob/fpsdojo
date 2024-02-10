638bcbfedb0447c69e2e4a55778a1e48a5af0e80
dastagirS <shroud464@gmail.com>
2024-02-11 04:18:52 +0530

    un-necessary code in test page

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,11 +1,8 @@
-import { getLatestAnime } from "@/lib/supabase/carosel";
 
 export default async function page() {
-
-  const bruh = await getLatestAnime()
-
   return (
     <>
+    TEST
     </>
   )
 }
